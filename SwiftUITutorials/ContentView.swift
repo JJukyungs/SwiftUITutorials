848e77957e9507e5257c2c905b781165f193013f
JJuKyungs <wnrud675@naver.com>
2022-09-19 13:40:24 +0900

    [섹션 6] Detail View 생성

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -13,6 +13,13 @@
     - Spacer를 이용하여 장치의 전체너비 사용하도록 하거나 상위 뷰의 모든 공간을 사용할 수 있게함
     - padding 을 이용해 약간의 공간을 추가
  
+    [섹션 6] Detail View
+    - UI 작성이 확실히 엄청 편리해진거 같기도 하다.
+    - View를 손쉽게 불러와서 작업을 할 수 있다.
+    - offset, padding 으로 view를 겹치게 하고 땡기게 할 수 있음
+    - Divider() 를 사용해 구분선을 생성할 수 있다.
+    - StackView를 좀 더 쉽게 사용할 수 있는 느낌?
+    
  
  
  */
@@ -20,21 +27,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                // Spacer() : 장치의 전체 너비를 사용하도록 하게 함
-                // 상위 뷰의 모든 공간을 사용
-                Text("California")
-                    .font(.subheadline)
+        VStack{
+            MapView()
+                .ignoresSafeArea(edges: .top) // safeArea까지 넣을 수 있게 해줌
+                .frame(height: 300) // 매개변수만 지정하면 너비에 맞게 자동 조정
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    // Spacer() : 장치의 전체 너비를 사용하도록 하게 함
+                    // 상위 뷰의 모든 공간을 사용
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider() // 구분선 넣기
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            // padding 약간의 공간 추가
+            .padding()
+            
+            // 외부 하단에 스페이서를 추가해 화면 상단으로 밀어넣기
+            Spacer()
         }
-        // padding 약간의 공간 추가
-        .padding()
     }
 }
 
