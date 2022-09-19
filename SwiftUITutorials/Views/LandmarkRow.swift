9d40708680daba4e2ac3869cc4d84122200b75e0
JJuKyungs <wnrud675@naver.com>
2022-09-19 14:55:58 +0900

    [섹션 2 - 2] ~ [섹션 2 - 3] Landmark Row View 생성

---
+++ SwiftUITutorials/Views/LandmarkRow.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkRow.swift
+//  SwiftUITutorials
+//
+//  Created by 이주경 on 2022/09/19.
+//
+
+/*
+    [센션 2 - 2] Row View 만들기
+    - landmark 세부 정보를 보여주는 행
+    - 나중에 여러 행을 랜드마크 목록으로 만듬
+    [섹션 2 - 3] Row PreView
+    - PreviewProvider
+    - 미리보기를 정확하게 랜더링
+    - Group 을 사용하여 묶어서 사용 가능
+ */
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
